f3ae7d9c2a03cb89a365149a420b7f00662352c0
shiellaworst <sajol.shiellamay.bscs2019@gmail.com>
2023-05-27 07:22:06 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Treasure-Shelf</title>
+
+    <!-- tailwind -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+<body class="bg-[#F2E5D0]"> 
+    
+<!--* Header-->
+<div class='bg-[#F2E5D0] mx-auto max-x-7xl  w-11/12 flex justify-between items-center h-[91px]'>
+  <!--* Left Side -->
+  <div class="flex items-center">
+  <img src="images/LOGO3BB.png" alt="logo" class="w-[80px] h-[80px]" />
+    
+  </div>
+ 
+  <!--* Right Side -->
+  <div class="flex gap-9">
+  <a class="inline-block text-black p-2 font-bold hover:text-[#FCA72B]" href='#'>About</a>
+  <a class="inline-block text-black p-2 font-bold hover:text-[#FCA72B]" href='#'>Contact Us</a>
+  <a class="inline-block py-2 px-8 text-black bg-[#FCA72B] rounded-[20px] font-bold hover:text-yellow-800 transition ease-in duration-150" href='#'>LOGIN</a>
+</div>
+
+</div>
+<!--* Banner -->
+<div class="md:flex md:justify-between py-40 px-0 bg-[#666161]">
+    <h2 class="text-6xl md:text-7xl lg:text-8xl text-white mb-3 font-bold text-center mx-auto p-4">BANNER</h2>
+</div>
+
+<div class="flex justify-center">
+  <div class="w-full">
+    <input type="text" class="border border-gray-300 bg-[#D9D9D9] rounded-[59px] px-4 py-2 w-full" placeholder="Search...">
+  </div>
+</div>
+
+
+
+
+<!--* Cards -->
+<div class="grid grid-cols-2 md:grid-cols-4 gap-3">
+                
+                    <a href="">
+                        <div class="max-w-sm rounded overflow-hidden shadow-lg bg-transparent h-100">
+                            <img class="w-full" src="images/LOGO3BB.png" alt="Image description">
+                            
+                        </div>
+                    </a>
+
+                    <a href="">
+                        <div class="max-w-sm rounded overflow-hidden shadow-lg bg-transparent  h-100">
+                            <img class="w-full" src="images/LOGO3BB.png" alt="Image description">
+                            
+                        </div>
+                    </a>
+
+                    <a href="">
+                        <div class="max-w-sm rounded overflow-hidden shadow-lg bg-transparent  h-100">
+                            <img class="w-full" src="images/LOGO3BB.png" alt="Image description">
+                            
+                        </div>
+                    </a>
+
+                    <a href="">
+                        <div class="max-w-sm rounded overflow-hidden shadow-lg bg-transparent  h-100">
+                            <img class="w-full" src="images/LOGO3BB.png" alt="Image description">
+                            
+                        </div>
+                    </a>
+
+            </div>
+
+<!--* Footer + Newsletters -->
+<div class="p-10 bg-[#FCA72B] text-indigo-200 md:flex md:justify-between md:items-center">
+  <!--* Left Side -->
+  
+  <!--* Right Side -->
+  <div class="flex items-center">
+    FOOTER
+  </div>
+</div>
+</body>
+</html>
